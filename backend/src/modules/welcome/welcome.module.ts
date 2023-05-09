af1fe74e20e19356bb8076fb712d4eae195e0c12
Arman Ghazaryan <arman.ghazaryan@hylink.am>
2023-05-09 21:00:47 +0400

    Remove project files for master branch

--- backend/src/modules/welcome/welcome.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WelcomeService } from './welcome.service';
-import {PrismaService} from "nestjs-prisma";
-import {WelcomeResolver} from "./welcome.resolver";
-
-@Module({
-  providers: [WelcomeService, PrismaService, WelcomeResolver]
-})
-export class WelcomeModule {}
--- frontend/components/Custom/NotReady/NotReady.tsx
+++
@@ -1,9 +0,0 @@
-const NotReady = () => {
-    return <div  className="not_ready_cont">
-        <h1>
-            Sorry, mobile version not ready yet!
-        </h1>
-      </div>
-}
-
-export default NotReady;
